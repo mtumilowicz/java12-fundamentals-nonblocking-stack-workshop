90c40905d64df660eed5a919f334f78a365c69b8
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-07 20:27:11 +0100

    tests draft

--- src/main/java/cas/CasCounter.java
+++ src/main/java/cas/CasCounter.java
@@ -1,7 +1,10 @@
 package cas;
 
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
 public class CasCounter {
-    private SimulatedCAS value;
+    private final SimulatedCAS value = new SimulatedCAS();
 
     public int getValue() {
         return value.get();
@@ -12,7 +15,27 @@ public class CasCounter {
         do {
             v = value.get();
         }
-        while (v != value.compareAndSwap(v, v + 1));
+        while (!value.compareAndSet(v, v + 1));
         return v + 1;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CasCounter cas = new CasCounter();
+        CountDownLatch latch = new CountDownLatch(5);
+        Runnable task = () -> {
+            IntStream.range(0, 1000).forEach(ignore -> cas.increment());
+            latch.countDown();
+        };
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        es.submit(task);
+        es.submit(task);
+        es.submit(task);
+        es.submit(task);
+        es.submit(task);
+        es.shutdownNow();
+
+        latch.await();
+
+        System.out.println(cas.getValue());
     }
 }
