522ee31c6e44d8fe8a4030b9e5a63e7972f13411
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-07 19:43:37 +0100

    SimulatedCAS.java formatting

--- src/main/java/cas/SimulatedCAS.java
+++ src/main/java/cas/SimulatedCAS.java
@@ -7,17 +7,15 @@ public class SimulatedCAS {
         return value;
     }
 
-    public synchronized int compareAndSwap(int expectedValue,
-                                           int newValue) {
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
         int oldValue = value;
-        if (oldValue == expectedValue)
+        if (oldValue == expectedValue) {
             value = newValue;
+        }
         return oldValue;
     }
 
-    public synchronized boolean compareAndSet(int expectedValue,
-                                              int newValue) {
-        return (expectedValue
-                == compareAndSwap(expectedValue, newValue));
+    public synchronized boolean compareAndSet(int expectedValue, int newValue) {
+        return (expectedValue == compareAndSwap(expectedValue, newValue));
     }
 }
