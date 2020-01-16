e8e59434431c3d915f9225d5980dafca412257ee
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-16 21:23:38 +0100

    tests refinement

--- src/test/groovy/stack/ConcurrentStackTest.groovy
+++ src/test/groovy/stack/ConcurrentStackTest.groovy
@@ -12,17 +12,17 @@ import java.util.stream.IntStream
 class ConcurrentStackTest extends Specification {
     def 'push is thread safe'() {
         given:
-        ConcurrentStack<String> cas = new ConcurrentStack<>()
+        ConcurrentStack<String> stack = new ConcurrentStack<>()
 
         and: 'stack is empty'
-        !cas.pop()
+        !stack.pop()
 
         and: 'latch to signalize end'
         CountDownLatch latch = new CountDownLatch(5)
 
         and: 'task that increments counter 1000 times'
         Runnable task = {
-            IntStream.range(0, 1000).forEach { ignore -> cas.push('a') }
+            IntStream.range(0, 1000).forEach { ignore -> stack.push('a') }
             latch.countDown()
         }
 
@@ -44,12 +44,12 @@ class ConcurrentStackTest extends Specification {
 
         and: 'count the elements by draining the stack'
         int i = 0
-        while (cas.pop() != null) {
+        while (stack.pop() != null) {
             i++
         }
 
         then: 'stack is empty again'
-        !cas.pop()
+        !stack.pop()
 
         and: 'exactly 5000 elements'
         i == 5000
@@ -65,11 +65,11 @@ class ConcurrentStackTest extends Specification {
         and: 'latch to signalize end'
         CountDownLatch latch = new CountDownLatch(5)
 
-        and: 'XXXXXXXXXXXXXXXXx'
+        and: 'concurrentSet for persisting stack elements'
         ConcurrentSkipListSet<Integer> concurrentSet = new ConcurrentSkipListSet<>()
 
         and: 'task that pops 1000 elements from the stack and push to the concurrentSet'
-        Runnable task2 = {
+        Runnable task = {
             IntStream.range(0, 1000).forEach { ignore -> concurrentSet.add(cas.pop()) }
             latch.countDown()
         }
@@ -81,11 +81,11 @@ class ConcurrentStackTest extends Specification {
         IntStream.range(0, 5000).forEach { cas.push(it) }
 
         when: 'run concurrently'
-        es.submit(task2)
-        es.submit(task2)
-        es.submit(task2)
-        es.submit(task2)
-        es.submit(task2)
+        es.submit(task)
+        es.submit(task)
+        es.submit(task)
+        es.submit(task)
+        es.submit(task)
 
         and: 'wait for all tasks to end'
         latch.await()
